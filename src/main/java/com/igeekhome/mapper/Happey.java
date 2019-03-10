14921b55b4e4a7c5808811b9143dcc94c11c1016
jiwei <jiwei@example.com>
2019-03-10 14:57:21 +0800

    二号

--- src/main/java/com/igeekhome/mapper/Happey.java
+++ src/main/java/com/igeekhome/mapper/Happey.java
@@ -2,6 +2,7 @@ package com.igeekhome.mapper;
 
 public class Happey {
 	public static void main(String[] args) {
-		System.out.println("Happey.main()");
+		System.out.println("Happey.main()2");
+		System.out.println("二号");
 	}
 }
